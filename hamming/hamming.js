8dd7c0588ad036618b1362b4d0cdb124f25eef99
Melvin Hernandez <m.hernandez@berkeley.edu>
2017-03-19 22:27:15 -0700

    Computing the hamming distance between two dna strands.

---
+++ hamming/hamming.js
@@ -0,0 +1,13 @@
+
+var Hamming = function() {};
+
+Hamming.prototype.compute = function(dna1, dna2) {
+	if (dna1.length !== dna2.length) throw new Error("DNA strands must be of equal length.");
+	let distance = 0;
+	for (let i = 0; i < dna1.length; i++) {
+		if (dna1[i] !== dna2[i]) distance++;
+	}
+	return distance;
+}
+
+module.exports = Hamming;
